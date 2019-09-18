eb2bf5b60541da501ed31644e8d86aeaf18881a2
zqnnn <775784772@qq.com>
2019-09-18 10:21:11 +0800

    堆排序

--- practice/32_spiralOrder.cpp
+++ practice/32_spiralOrder.cpp
@@ -18,60 +18,111 @@ using namespace std;
 
 //解决思路: 向右走到底，换方向向下走到底，走完一圈后，更新起始点位置和终止点位置，重复上述过程
 
-class Solution {
-public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vector<int> result;
-        if (matrix.size() == 0)
-            return result;
-        int startI = 0, startJ = 0;
-        int endI = matrix.size();
-        int endJ = matrix[0].size();
-        int i = 0,j = 0;
-        while (true)
-        {
-            for (; j <endJ; ++j)  //向右走到尽头
-                result.push_back(matrix[i][j]);
-            ++i;
-            j = endJ - 1;
-            if (!(i < endI))
-                return result;
+//class Solution {
+//public:
+//    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+//        vector<int> result;
+//        if (matrix.size() == 0)
+//            return result;
+//        int startI = 0, startJ = 0;
+//        int endI = matrix.size();
+//        int endJ = matrix[0].size();
+//        int i = 0,j = 0;
+//        while (true)
+//        {
+//            for (; j <endJ; ++j)  //向右走到尽头
+//                result.push_back(matrix[i][j]);
+//            ++i;
+//            j = endJ - 1;
+//            if (!(i < endI))
+//                return result;
+//
+//            for (; i < endI; ++i)  //向下走到尽头
+//                result.push_back(matrix[i][j]);
+//            i = endI - 1;
+//            --j;
+//            if (!(j >= startJ))
+//                return result;
+//
+//            for (; j >= startJ; --j)  //向左走到尽头
+//                result.push_back(matrix[i][j]);
+//            --i;
+//            j = startJ;
+//            if (!(i > startI))
+//                return result;
+//
+//            for (; i > startI; --i)  //向上走到尽头
+//                result.push_back(matrix[i][j]);
+//
+//            //尽头位置和起始位置更新
+//            ++startI, ++startJ;
+//            --endI,--endJ;
+//            i = startI, j = startJ;
+//
+//            if (startI >= endI || startJ >= endJ)
+//                break;
+//        }
+//        return result;
+//    }
+//};
 
-            for (; i < endI; ++i)  //向下走到尽头
-                result.push_back(matrix[i][j]);
-            i = endI - 1;
-            --j;
-            if (!(j >= startJ))
-                return result;
+//逆时针
 
-            for (; j >= startJ; --j)  //向左走到尽头
-                result.push_back(matrix[i][j]);
-            --i;
-            j = startJ;
-            if (!(i > startI))
-                return result;
+vector<int> printMatrix(vector<vector<int>> matrix) {
+    int rows = matrix.size();
+    int cols = matrix[0].size();
+    vector<int> result;
 
-            for (; i > startI; --i)  //向上走到尽头
-                result.push_back(matrix[i][j]);
-
-            //尽头位置和起始位置更新
-            ++startI, ++startJ;
-            --endI,--endJ;
-            i = startI, j = startJ;
-
-            if (startI >= endI || startJ >= endJ)
-                break;
-        }
+    if(rows == 0 && cols == 0){
         return result;
     }
-};
+    int left = 0, right = cols - 1, top = 0, bottom = rows - 1;
+
+    while(left <= right && top <= bottom){
+        //从上到下
+        for(int i = top; i <= bottom; ++i){
+            result.push_back(matrix[i][left]);
+        }
+        //从左到右
+        for(int i = left+1; i <= right; ++i){
+            result.push_back(matrix[bottom][i]);
+        }
+        //从下到上
+        if(left != right){
+            for(int i = bottom - 1; i >= top; --i){
+                result.push_back(matrix[i][right]);
+            }
+        }
+        //从右到左
+        if(top != bottom){
+            for(int i = right - 1; i > left; --i){
+                result.push_back(matrix[top][i]);
+            }
+        }
+        left++, top++, right--, bottom--;
+    }
+    return result;
+}
+
 
 int main(){
-    Solution s;
-    vector<vector<int>> ma = {{1,2,3},{3,4,5}};
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> ma;
+    for(int i = 0; i < n; i++){
+        vector<int> sub;
+        for(int j = 0; j < m; j++){
+            int tmp;
+            cin >> tmp;
+            sub.push_back(tmp);
+        }
+        ma.push_back(sub);
+    }
 
-    vector<int> a = s.spiralOrder(ma);
+    vector<int> a = printMatrix(ma);
 
-    for(int i = 0; i < a.size(); i++)
-        cout<<a[i]<<endl;
+    for(int i = 0; i < a.size()-1; i++)
+        cout<<a[i]<<' ';
+
+    cout << a[a.size()-1];
 }
